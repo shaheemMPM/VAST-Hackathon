aab48fa95c40bd0a83557e88d27e02102bf7cd57
shaheemMPM <shaheem.mpm@gmail.com>
2019-02-09 02:49:07 +0530

    Publish : chrome extension v1.0

---
+++ digital-wellbeing/intercept.js
@@ -0,0 +1,35 @@
+// let useTime;
+//
+// chrome.storage.sync.get(['myTime'], function(result) {
+//     useTime = result.myTime;
+//     console.log("Time : "+useTime);
+// });
+//
+// if (useTime === null) {
+//   useTime = 500000;
+// }
+
+setTimeout(function(){
+  var x = document.querySelectorAll('[role="feed"]');
+  var y = getRandomInt(0, 9);
+  x[0].innerHTML = "<h1 style=\"font-size: 30px;\">"+quotes[y]+"</h1>";
+},5000);
+
+var quotes = [
+  "Better three hours too soon, than one minute too late.<br> - William Shakespeare",
+  "Time is the wisest counselor of all.<br> - Pericles",
+  "Time is the school in which we learn, time is the fire in which we burn.<br> - Delmore Schwartz",
+  "Nothing is a waste of time if you use the experience wisely.<br> - Rodin",
+  "Time as he grows old teaches many lessons.<br> - Rodin",
+  "Time as he grows old teaches many lessons.<br> - Aeschylus",
+  "Histories make men wise.<br> - Francis Bacon",
+  "Time is what we want most, but what we use worst.<br> - William Penn",
+  "The common man is not concerned about the passage of time, the man of talent is driven by it.<br> - Shoppenhauer",
+  "Time = life; therefore, waste your time and waste of your life, or master your time and master your life.<br> - Alan Lakein"
+];
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ digital-wellbeing/js/mock.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-mockexp').onclick = function () {
+  document.getElementById('btn-expcsv').click();
+}
